0d618e931aa97ccbea6bd1d33f81f96343a7100e
mpk75234 <mikekennedy75234@gmail.com>
2020-10-26 13:02:52 -0500

    added POST validation function

--- app.py
+++ app.py
@@ -4,19 +4,34 @@ from flask_restful import Api,Resource
 app  = Flask(__name__)
 api = Api(app)
 
+def validatePost(data, function):
+  if(function == "add"):
+      if "x" not in data or "y" not in data:
+          return 301
+      else:
+          return 200
+
 class Add(Resource):
     def post(self):
         data = request.get_json()
-        x = data["x"]
-        y = data["y"]
-        x = int(x)
-        y = int(y)
-        ret = x + y
-        retMap = {
-            "Message" : ret,
-            "status" : 200
-        }
-        return jsonify(retMap)
+        status_code = validatePost(data, "add")
+        if (status_code != 200):
+            retJson = {
+                "Message" : "Operator Error",
+                "status" : status_code
+            }
+            return jsonify(retJson)
+        else:
+            x = data["x"]
+            y = data["y"]
+            x = int(x)
+            y = int(y)
+            ret = x + y
+            retMap = {
+                "Message" : ret,
+                "status" : 200
+            }
+            return jsonify(retMap)
 
 class Subtract(Resource):
     def post(self):
